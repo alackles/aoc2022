00ae1149f956d1e444b915c09032b600867b181e
Acacia Ackles <alackles@msu.edu>
2022-12-05 21:08:16 -0600

    Add comments

--- day5/pt1_pt2.py
+++ day5/pt1_pt2.py
@@ -1,42 +1,51 @@
 import re 
 
+
 def chunk(s, n):
+    # Splits a string s into chunks of length n
     return [s[i:i+n] for i in range(0, len(s), n)]
 
 def read_crates(crates, width=4):
-    crates = crates.replace('[',' ').replace(']',' ').split('\n')
-    num_stacks = int(crates.pop().split()[-1])
-    crates = [chunk(crate, width) for crate in crates]
-    crates.reverse()
-    stacks = [[] for _ in range(num_stacks)]
-    for i in range(num_stacks):
+    # Parse crates section of input
+    crates = crates.replace('[',' ').replace(']',' ').split('\n')       # turn all brackets into more whitespace
+    num_stacks = int(crates.pop().split()[-1])                          # take off the last row (numbers) & return the number of stacks
+    crates = [chunk(crate, width) for crate in crates]                  # create the rows of crates, top to bottom
+    crates.reverse()                                                    # flip the rows, so now we can read bottom to top
+    stacks = [[] for _ in range(num_stacks)]                            # initialize num_stacks empty stacks
+    for i in range(num_stacks):                                         # loop through first stack, bottom to top; then second stack, bottom to top, etc
         for layer in crates:
             if layer[i].strip():
-                stacks[i].append(layer[i].strip())
-    return stacks
+                stacks[i].append(layer[i].strip())                      # add to the stack whenever there's a letter
+    return stacks                                                       
 
 def read_directions(directions):
+    # Parse directions section of input
     directions = directions.split('\n')
     return [re.findall(r'\d+', dir) for dir in directions]
 
 def move_crates(stacks, dirs, cratemover9001=True):
+    # Perform the actual movements
     for dir in dirs:
         n, start, end = [int(x) for x in dir]
         if cratemover9001:
-            stacks[end-1] += stacks[start-1][-n:]
-            del stacks[start-1][-n:]
+            # Part 2
+            stacks[end-1] += stacks[start-1][-n:]               # Append the last n elements of the start stack to the end stack
+            del stacks[start-1][-n:]                            # Delete the last n elements of the start stack
         else:
+            # Part 1
             for _ in range(n):
-                stacks[end-1].append(stacks[start-1].pop())
+                stacks[end-1].append(stacks[start-1].pop())     # Pop off the start stack and push onto the end stack
 
 def get_top(stacks):
-    print("".join([stack[-1] for stack in stacks]))
+    # Convert to puzzle input format
+    print("".join([stack[-1] for stack in stacks]))             # Gets the last element of each stack
         
 
+# Read file
 with open("input.txt") as f:
     crates, directions = f.read().split('\n\n')
-    stacks = read_crates(crates)
-    dirs = read_directions(directions)
 
+stacks = read_crates(crates)
+dirs = read_directions(directions)
 move_crates(stacks, dirs)
 get_top(stacks)
